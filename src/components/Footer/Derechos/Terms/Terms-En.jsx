2e056f2c47634bf643ae3f57de5f35d0b2536dde
Emanuel Escudero <escudero.mdz@gmail.com>
2023-06-16 00:01:55 -0300

    Punto de Control: Términos y Condiciones

--- src/components/Footer/Derechos/Terms/Terms-En.jsx
+++ src/components/Footer/Derechos/Terms/Terms-En.jsx
@@ -3,26 +3,37 @@ import Logo from '../../../Header/Logo/Logo';
 import './Terms.css';
 import termsEnData from '../../../../translations/en/termsEn.json';
 
-const TermsEn = () => {
+const TermsEs = () => {
   const termsEn = termsEnData;
 
   return (
-    <div id='terms-es' className='terms'>
+    <div id='terms-en' className='terms'>
       <div className='termsContainer'>
         <div className='headerTerms'>
           <Logo />
         </div>
         <div className='bodyTerms'>
           <h3 className='title'>{termsEn.title}</h3>
-          {Object.values(termsEn.text).map((text, id) => (
-            <p className='text' key={id}>
-              {text}
-            </p>
-          ))}
+          <h4>{termsEn.text.title1}</h4>
+          <p>{termsEn.text.text1}</p>
+          <h4>{termsEn.text.title2}</h4>
+          <p>{termsEn.text.text2}</p>
+          <h4>{termsEn.text.title3}</h4>
+          <p>{termsEn.text.text3}</p>
+          <h4>{termsEn.text.title4}</h4>
+          <p>{termsEn.text.text4}</p>
+          <h4>{termsEn.text.title5}</h4>
+          <p>{termsEn.text.text5}</p>
+          <h4>{termsEn.text.title6}</h4>
+          <p>{termsEn.text.text6}</p>
+          <h4>{termsEn.text.title7}</h4>
+          <p>{termsEn.text.text7}</p>
+          <h4>{termsEn.text.title8}</h4>
+          <p>{termsEn.text.text8}</p>
         </div>
       </div>
     </div>
   );
 };
 
-export default TermsEn;
+export default TermsEs;
--- src/components/Footer/Derechos/Terms/Terms-Es.jsx
+++ src/components/Footer/Derechos/Terms/Terms-Es.jsx
@@ -14,11 +14,22 @@ const TermsEs = () => {
         </div>
         <div className='bodyTerms'>
           <h3 className='title'>{termsEs.title}</h3>
-          {Object.values(termsEs.text).map((text, id) => (
-            <p className='text' key={id}>
-              {text}
-            </p>
-          ))}
+          <h4>{termsEs.text.title1}</h4>
+          <p>{termsEs.text.text1}</p>
+          <h4>{termsEs.text.title2}</h4>
+          <p>{termsEs.text.text2}</p>
+          <h4>{termsEs.text.title3}</h4>
+          <p>{termsEs.text.text3}</p>
+          <h4>{termsEs.text.title4}</h4>
+          <p>{termsEs.text.text4}</p>
+          <h4>{termsEs.text.title5}</h4>
+          <p>{termsEs.text.text5}</p>
+          <h4>{termsEs.text.title6}</h4>
+          <p>{termsEs.text.text6}</p>
+          <h4>{termsEs.text.title7}</h4>
+          <p>{termsEs.text.text7}</p>
+          <h4>{termsEs.text.title8}</h4>
+          <p>{termsEs.text.text8}</p>
         </div>
       </div>
     </div>
