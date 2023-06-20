4174d9c41106eda23115888a22e0ec16107ca3b7
Emanuel Escudero <escudero.mdz@gmail.com>
2023-06-20 03:14:32 -0300

    Cambio offset en elementos Link

--- src/components/Footer/Datos/Datos.jsx
+++ src/components/Footer/Datos/Datos.jsx
@@ -70,7 +70,7 @@ const Datos = () => {
                 activeSection === 'aboutUs' ? 'active' : ''
               }`}
               to='aboutUs'
-              offset={-100}
+              offset={-94}
               spy={true}
               smooth={true}
               duration={500}
@@ -82,7 +82,7 @@ const Datos = () => {
                 activeSection === 'services' ? 'active' : ''
               }`}
               to='services'
-              offset={-100}
+              offset={-94}
               spy={true}
               smooth={true}
               duration={500}
@@ -94,7 +94,7 @@ const Datos = () => {
                 activeSection === 'contact' ? 'active' : ''
               }`}
               to='contact'
-              offset={-100}
+              offset={-94}
               spy={true}
               smooth={true}
               duration={500}
