816a022da4940d7b01b5c2e827efcb70d3e0a0d8
Emanuel Escudero <escudero.mdz@gmail.com>
2023-06-20 00:45:12 -0300

    Cambio de email en el form de contacto

--- src/components/Footer/Datos/Datos.jsx
+++ src/components/Footer/Datos/Datos.jsx
@@ -58,7 +58,7 @@ const Datos = () => {
                 activeSection === 'home' ? 'active' : ''
               }`}
               to='home'
-              offset={-100}
+              offset={-95}
               spy={true}
               smooth={true}
               duration={500}
@@ -70,7 +70,7 @@ const Datos = () => {
                 activeSection === 'aboutUs' ? 'active' : ''
               }`}
               to='aboutUs'
-              offset={-100}
+              offset={-95}
               spy={true}
               smooth={true}
               duration={500}
@@ -82,7 +82,7 @@ const Datos = () => {
                 activeSection === 'services' ? 'active' : ''
               }`}
               to='services'
-              offset={-100}
+              offset={-95}
               spy={true}
               smooth={true}
               duration={500}
@@ -94,7 +94,7 @@ const Datos = () => {
                 activeSection === 'contact' ? 'active' : ''
               }`}
               to='contact'
-              offset={-100}
+              offset={-95}
               spy={true}
               smooth={true}
               duration={500}
